0c4b2f34ce508bf8564259ba4cfffc1b3c6491ef
snigdhapodugu <165712664+snigdhapodugu@users.noreply.github.com>
2025-01-30 19:15:35 -0800

    Lab 4 Index Push

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { fetchJSON, renderProjects } from '../global.js';
-import { fetchGitHubData } from '../global.js';
+import { fetchJSON, renderProjects } from '/global.js';
+import { fetchGitHubData } from '/global.js';
 
-const projects = await fetchJSON('../lib/projects.json');
+const projects = await fetchJSON('/lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
