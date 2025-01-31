e1732eef62da90f5214af0bd43cd4d87b30e2dde
snigdhapodugu <165712664+snigdhapodugu@users.noreply.github.com>
2025-01-30 19:10:43 -0800

    Lab 4 Another Push

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
-import { fetchGitHubData } from './global.js';
+import { fetchGitHubData } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
 const latestProjects = projects.slice(0, 3);
